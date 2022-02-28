ad6dc0cfe987c0f64c8fe28a95afee9f0de158ca
Eric Berquist <eric.berquist@gmail.com>
2022-02-28 11:00:43 -0500

    Armadillo: test stubs

---
+++ test/contrib/arma/arma_test.cpp
@@ -0,0 +1,16 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <h5cpp/hdf5.hpp>
+#include <h5cpp/contrib/arma/arma.hpp>
+
+
+using namespace hdf5;
+
+
+
+SCENARIO("arma") {
+    GIVEN("arma") {
+        THEN("arma") {
+        }
+    }
+}
