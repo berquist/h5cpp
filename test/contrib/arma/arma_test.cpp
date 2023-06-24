f7b7ccc748a8a95956affd19cf5f1c220f52a259
Eric Berquist <eric.berquist@gmail.com>
2023-06-24 17:05:33 -0400

    Catch2 fixes

--- test/contrib/arma/arma_test.cpp
+++ test/contrib/arma/arma_test.cpp
@@ -1,5 +1,9 @@
+#ifdef H5CPP_CATCH2_V2
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+#else
+#include <catch2/catch_all.hpp>
+#endif
 #include <h5cpp/hdf5.hpp>
 #include <h5cpp/contrib/arma/arma.hpp>
 
